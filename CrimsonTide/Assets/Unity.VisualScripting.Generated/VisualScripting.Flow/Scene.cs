e99daa5f6727d668784982d8dca98f39e0f50d23
3entJem <livyjreed702@gmail.com>
2025-02-03 18:57:46 -0600

    Simple Buttons and player movements code

---
+++ CrimsonTide/Assets/Unity.VisualScripting.Generated/VisualScripting.Flow/Scene.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Game1()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void StartScene()
+    {
+        SceneManager.LoadScene("Start");
+    }
+}
