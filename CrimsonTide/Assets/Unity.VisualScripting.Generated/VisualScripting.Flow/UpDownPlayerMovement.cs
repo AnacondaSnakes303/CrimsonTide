717cc306f5938cd27e2499e3e2cd780e8154bad2
3entJem <livyjreed702@gmail.com>
2025-03-10 18:55:05 -0500

    Changes to Camera, Player Movement, and Colliders

--- CrimsonTide/Assets/Unity.VisualScripting.Generated/VisualScripting.Flow/UpDownPlayerMovement.cs
+++ CrimsonTide/Assets/Unity.VisualScripting.Generated/VisualScripting.Flow/UpDownPlayerMovement.cs
@@ -1,30 +1,24 @@
 using UnityEngine;
 using System.Collections;
 
+
 public class UpDownPlayerMovement : MonoBehaviour
 {
-    public float speed;
+    private float horizontalInput;
+    float verticalInput;
     public Collision2D collision;
     private Transform Player;
 
+    [SerializeField] 
+    float speed;
+
     private void Update()
     {
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.Translate(Vector2.right * speed);
-        }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.Translate(-Vector2.right * speed);
-        }
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            transform.Translate(Vector2.up * speed);
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            transform.Translate(-Vector2.up * speed);
-        }
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector2.right * horizontalInput * speed);
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector2.up * verticalInput * speed);
     }
     
 }
